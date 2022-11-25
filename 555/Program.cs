93c5912953941c08c75616fd74af409d53627ea6
pr0gmatik <hozianinki@gmail.com>
2022-11-25 20:03:26 +0300

    1

--- 555/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿ Console.WriteLine("Введите число:  ");
- int n = Convert.ToInt32(Console.ReadLine());
- int num = -n;
- while (num <= n)
- {
-     Console.Write($"{num} ");
-     num++;
- }
- string user = Console.ReadLine();
